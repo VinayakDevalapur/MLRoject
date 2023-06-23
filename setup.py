6ef9cc8c67a5313d7cabb839a951f485d7e33ec1
VinayakDevalapur <vinayak.devalapur22@gmail.com>
2023-06-23 11:02:02 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
-def get_requirements_list():
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Vinayak Devalapur"
+DESRCIPTION="This is a first FSDS Project"
 
-    """this function is going to return list of requirements mentioned in requirements.txt
-    this function is going to return list which contains name of libraries in requirements,txt"""
+REQUIREMENT_FILE_NAME="requirements.txt"
 
-    with open (REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
 
-# declaring variables for setup functions
-PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
-AUTHOR = "Vinayak D"
-DESCRIPTION = "First Project"
-PACKAGES = ["housing"]
-REQUIREMENT_FILE_NAME = "requirements.txt"
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove("-e .")
+
 
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
-description=DESCRIPTION,
-packages=PACKAGES, 
+description=DESRCIPTION,
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
